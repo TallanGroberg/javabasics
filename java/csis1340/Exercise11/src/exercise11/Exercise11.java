18fe749ef7ddf52f1cc6226a67e4ba5ad4ee3829
TallanGroberg <tallan.taven@gmail.com>
2020-11-17 20:26:12 -0500

    add refactors

--- java/csis1340/Exercise11/src/exercise11/Exercise11.java
+++ java/csis1340/Exercise11/src/exercise11/Exercise11.java
@@ -1,29 +1,27 @@
 
 package exercise11;
-import java.util.Scanner;
 /**
  *
  * @author Tallan Groberg
  */
-public class Exercise11 {
-
+public class Exercise11 
+{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
-        Scanner input = new Scanner(System.in);
+        Keyboard input = new Keyboard();
         System.out.println("a number greater than 0");
 
-        int N = input.nextInt();
-        int counter = 0;
-        System.out.println("your number is: " + N);
-        while((N + 1) > counter) {
+        int n = input.readInt();
+        int counter = 1;
+        System.out.println("your number is: " + n);
+        while((n + 1) > counter) 
+        {
             System.out.println(counter);
             counter++;
-           boolean y = (5< 3);
-           System.out.println(y);
-
         }
     }
 }
